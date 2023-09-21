5d0979ba6020ece1b5102c0592b2917fce42f19e
sakshi-hattikudur <145516232+sakshi-hattikudur@users.noreply.github.com>
2023-09-20 21:33:10 -0400

    Add files via upload

---
+++ Canada_Immigration_Queries/README.md
@@ -0,0 +1,10 @@
+This dataset has been obtained from the following link:
+
+https://www.kaggle.com/datasets/ammaraahmad/immigration-to-canada
+
+This dataset contains information of immigrants to the country of Canada.
+
+I am making an attempt to query this dataset using SQL and find some interesting insights. I hope you will find them useful.
+
+Thank you,
+Rupak Hattikudur
---
+++ Canada_Immigration_Queries/Regions_count_descending.sql
@@ -0,0 +1,33 @@
+-- Query to find the regions' count to descending order for Github 
+
+select region,count(*) from can_imm.canada_data
+group by region
+order by count(*) desc;
+
+-- This query shows that Western Asia is the region from where most immigrants are coming to Canada (country-wise)
+
+/*
+Western Asia	18
+Eastern Africa	17
+Western Africa	16
+Southern Europe	14
+Caribbean	13
+South America	12
+Eastern Europe	10
+South-Eastern Asia	10
+Northern Europe	10
+Southern Asia	9
+Middle Africa	9
+Western Europe	9
+Central America	8
+Northern Africa	7
+Eastern Asia	7
+Southern Africa	5
+Central Asia	5
+Polynesia	4
+Melanesia	4
+Micronesia	4
+Australia and New Zealand	2
+Northern America	2
+
+*/
